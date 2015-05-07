d5cc27528140a6f450a6726b65765e3992e91519
Jan Stavěl <stavel.jan@gmail.com>
2015-05-07 12:02:12 +0200

    jstavel/edeposit#337 doplnil jsem policka eperiodika a pripravil jsem ohlasovaci formular

--- edeposit/content/eperiodical.py
+++ edeposit/content/eperiodical.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from five import grok
 
 from z3c.form import group, field
@@ -5,6 +6,7 @@ from zope import schema
 from zope.interface import invariant, Invalid
 from zope.schema.interfaces import IContextSourceBinder
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+from z3c.form import group, field, button
 
 from plone.dexterity.content import Container
 from plone.directives import dexterity, form
@@ -15,11 +17,11 @@ from plone.namedfile.interfaces import IImageScaleTraversable
 from z3c.relationfield.schema import RelationChoice, RelationList
 from plone.formwidget.contenttree import ObjPathSourceBinder, UUIDSourceBinder
 from plone.formwidget.autocomplete import AutocompleteFieldWidget, AutocompleteMultiFieldWidget
-
+from plone.dexterity.utils import createContentInContainer, addContentToContainer, createContent
 from edeposit.content.library import ILibrary
-
+from edeposit.content.eperiodicalfolder import IePeriodicalFolder
 from edeposit.content import MessageFactory as _
-
+from plone import api
 
 # Interface class; used to define content-type schema.
 
@@ -27,140 +29,36 @@ class IePeriodical(form.Schema, IImageScaleTraversable):
     """
     E-Deposit - ePeriodical
     """
-    subtitle = schema.ASCIILine (
-        title = _(u"Subtitle"),
-        required = False,
-        )
+    title = schema.TextLine(
+        title=u'Název periodického tisku',
+        required=True
+    )
 
-    edition = schema.ASCIILine (
-        title = _(u"Edition"),
-        required = False,
-        )
+    issn = schema.ASCIILine (
+        title = u"ISSN",
+        required = True,
+    )
 
-    form.fieldset('Publishing',
-                  label=_(u"Publishing"),
-                  fields = [ 'publisher',
-                             'published_with_coedition',
-                             'published_at_order',
-                             'place_of_publishing',
-                             ]
-                  )
-    place_of_publishing = schema.ASCIILine (
-        title = _(u"Place of Publishing"),
-        required = False,
-        )
+    cetnost_vydani = schema.TextLine (
+        title = u"Četnost (periodicita) vydání",
+        required = True,
+    )
 
-    publisher = schema.ASCIILine (
-        title = _(u"Publisher"),
-        required = False,
-        )
+    misto_vydani = schema.TextLine (
+        title = u"Místo vydáni",
+        required = True,
+    )
 
-    published_with_coedition = schema.ASCIILine(
-        title = _(u'Published with Coedition'),
-        description = _(u'Fill in a coedition of an ePublication'),
+    obsahove_zamereni = schema.TextLine (
+        title = u"Obsahové zaměření",
         required = False,
-        readonly = False,
-        default = None,
-        missing_value = None,
-        )
+    )
 
-    published_at_order = schema.ASCIILine(
-        title = _(u'Published at order'),
-        description = _(u'Fill in an order an ePublication was published at.'),
-        required = False,
-        readonly = False,
-        default = None,
-        missing_value = None,
-        )
-    
-
-    form.fieldset('volume',
-                  label=_(u'Volume'),
-                  fields = ['volume','volume_title','volume_number']
-                  )
-    volume = schema.ASCIILine (
-        title = _(u"Volume"), # svazek
-        required = False,
-        )
-    
-    volume_title = schema.ASCIILine (
-        title = _(u"Volume Title"),
-        required = False,
-        )
-    
-    volume_number = schema.ASCIILine (
-        title = _(u"Volume Number"),
-        required = False,
-        )
-
-    form.fieldset('technical',
-                  label=_('Technical'),
-                  fields = [ 'person_who_processed_this',
-                             'aleph_doc_number',
-                             ]
-                  )
-    person_who_processed_this = schema.ASCIILine(
-        title = _(u'Person who processed this.'),
-        description = _(u'Fill in a name of a person who processed this ePeriodical.'),
-        required = False,
-        readonly = False,
-        default = None,
-        missing_value = None,
-        )
-
-    aleph_doc_number = schema.ASCIILine(
-        title = _(u'Aleph DocNumber'),
-        description = _(u'Internal DocNumber that Aleph refers to metadata of this ePeriodical'),
-        required = False,
-        readonly = False,
-        default = None,
-        missing_value = None,
-        )
-
-    form.fieldset('accessing',
-                  label=_(u'Accessing'),
-                  fields = ['libraries_that_can_access_at_library_terminal',
-                            'libraries_that_can_access_at_public',
-                            ])
-    #form.widget(libraries_that_can_access_at_library_terminal=AutocompleteMultiFieldWidget)    
-    libraries_that_can_access_at_library_terminal = RelationList(
-        title = _(u'Libraries that can access at library terminal'),
-        description = _(u'Choose libraries that can show an ePublication at its terminal.'),
-        required = False,
-        readonly = False,
-        default = [],
-        value_type = RelationChoice(
-            title = _(u'Related libraries'),
-            source = ObjPathSourceBinder(object_provides=ILibrary.__identifier__),
-            )
-        )
-    #form.widget(libraries_that_can_access_at_public=AutocompleteMultiFieldWidget)    
-    libraries_that_can_access_at_public = RelationList(
-        title = _(u'Libraries that can access at public'),
-        description = _(u'Choose libraries that can show an ePublication at public.'),
-        required = False,
-        readonly = False,
-        default = [],
-        value_type = RelationChoice(
-            title = _(u'Related libraries at public'),
-            source = ObjPathSourceBinder(object_provides=ILibrary.__identifier__),
-            )
-        )
-
-    form.fieldset('riv',
-                  label=_(u'RIV'),
-                  fields = ['category_for_riv',
-                            ])
-
-    category_for_riv = schema.ASCIILine(
-        title = _(u'RIV category'),
-        description = _(u'Category of an ePublication for RIV'),
-        required = False,
-        readonly = False,
-        default = None,
-        missing_value = None,
-        )
-
+    description = schema.Text(
+        title=u"Poznámka",
+        required=False,
+        missing_value=u'',
+    )
 
 
 # Custom content-type class; objects created for this content type will
@@ -184,12 +82,38 @@ class ePeriodical(Container):
 # of this type by uncommenting the grok.name line below or by
 # changing the view class name and template filename to View / view.pt.
 
-class SampleView(grok.View):
-    """ sample view class """
+#  http://www.mkcr.cz/scripts/detail.php?id=353
 
-    grok.context(IePeriodical)
-    grok.require('zope2.View')
+class AddAtOnceForm(form.SchemaForm):
+    grok.name('add-at-once')
+    grok.require('edeposit.AddEPeriodical')
+    grok.context(IePeriodicalFolder)
+    schema = IePeriodical
+    ignoreContext = True
+    label = u"Oznámení vydávání periodického tisku"
+    enable_form_tabbing = False
+    autoGroups = False
+    
+    @button.buttonAndHandler(u"Odeslat", name='save')
+    def handleAdd(self, action):
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
 
-    # grok.name('view')
+        newEPeriodical = createContentInContainer(self.context, 'edeposit.content.eperiodical', **data)
+        wft = api.portal.get_tool('portal_workflow')
+        api.content.get_state(newEPeriodical)
+        wft.doActionFor(newEPeriodical, 'toAcquisition', comment='handled automatically')
+        self.request.response.redirect(newEPeriodical.absolute_url())
 
-    # Add view methods here
+
+# class SampleView(grok.View):
+#     """ sample view class """
+
+#     grok.context(IePeriodical)
+#     grok.require('zope2.View')
+
+#     # grok.name('view')
+
+#     # Add view methods here
