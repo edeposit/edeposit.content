8961089880e32939ea64be4c79283e4047b048c7
Jan Stavěl <stavel.jan@gmail.com>
2015-08-05 16:29:00 +0200

    jstavel/edeposit#476 doplnil jsem sablonu pro widget a nastavil jsem pravidla pro zobrazeni linky ke stazeni.

--- edeposit/content/widgets/originalfilesource.py
+++ edeposit/content/widgets/originalfilesource.py
@@ -17,7 +17,26 @@ class OriginalFileSourceWidget(NamedFileWidget):
     def can_change(self):
         mtool = api.portal.get_tool('portal_membership')
         return mtool.checkPermission('Modify portal content', self.context)
-    pass
+
+    @property
+    def can_download(self):
+        return self.context.is_public or self.can_change
+
+    @property
+    def storage_download_url(self):
+        return getattr(self.context.storage_download_url,None)
+
+    @property
+    def storage_filename(self):
+        return getattr(self.context.storage_download_url,None)
+
+    @property
+    def filename(self):
+        return super(OriginalFileSourceWidget,self).filename or self.storage_filename
+
+    @property
+    def download_url(self):
+        return super(OriginalFileSourceWidget,self).download_url or self.storage_download_url
 
 @implementer(IFieldWidget)
 @adapter(IOriginalFileSourceField, IFormLayer)
